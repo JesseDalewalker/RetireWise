5f8ce8afe0562c7a1f90e8240dddfa31b6243098
Daniel Maher <danielmaher01@gmail.com>
2024-02-16 17:12:25 -0500

    created button and routing to route to matching game page

--- RetireWise/src/app/video-and-quizzes/video-and-quizzes.component.ts
+++ RetireWise/src/app/video-and-quizzes/video-and-quizzes.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-video-and-quizzes',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './video-and-quizzes.component.html',
-  styleUrl: './video-and-quizzes.component.css'
+  styleUrl: './video-and-quizzes.component.css',
 })
-
-export class VideoAndQuizzesComponent {
-
-}
+export class VideoAndQuizzesComponent {}
