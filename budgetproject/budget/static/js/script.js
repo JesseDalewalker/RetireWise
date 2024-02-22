f4be2c0e476438078c042a094afd6c55b9831490
day-vo <59674674+day-vo@users.noreply.github.com>
2024-02-22 00:11:11 -0500

    budget app project

---
+++ budgetproject/budget/static/js/script.js
@@ -0,0 +1,25 @@
+
+(function(){
+
+    document.querySelector('#categoryInput').addEventListener('keydown', function(e){
+        if (e.keyCode !=13){
+            return;
+        }
+
+        e.preventDefault()
+
+        var categoryName = this.value
+        this.value = ''
+        addNewCategory(categoryName)
+        updateCategoriesString()
+    })
+
+    function addNewCategory(name){
+
+        document.querySelector('#categoriesContainer').insertAdjacentHTML('beforeend',
+        `<li class="category">
+            <span class="name">${name}</span>
+            <span onclick="removeCategory(this)" class="btnRemove bold">X</span>
+        </li>`)
+    }
+})()
---
+++ budgetproject/budget/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Project
+from django.views.generic import CreateView
+
+
+def project_list(request):
+    return render(request, 'budget/project-list.html')
+
+def project_detail(request, project_slug):
+    project = get_object_or_404(Project, slug=project_slug)
+    return render(request, 'budget/project-detail.html', {'project': project, 'expense_list': project.expenses.all()})
+
+
+class ProjectCreateView(CreateView):
+    model = Project
+    template_name = 'budget/add-project.html'
+    fields = ('name', 'budget')
