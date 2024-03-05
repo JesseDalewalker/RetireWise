fab1d7c5092ad496eee377ab32be8ea562c79546
Daniel Maher <danielmaher01@gmail.com>
2024-03-05 17:47:26 -0500

    updated gitignore

--- RetireWise/backend/src/term.ts
+++ RetireWise/backend/src/term.ts
@@ -2,7 +2,7 @@ import * as mongodb from "mongodb";
 
 export interface Term {
     _id?: mongodb.ObjectId;
-    wordname: string;
+    wordName: string;
     definitionID: string;
     isFlipped: boolean;
     state: string;
