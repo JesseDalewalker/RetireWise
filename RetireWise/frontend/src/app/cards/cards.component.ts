162f2d945ec7d1ee9623694227e699470367bfe9
Daniel Maher <danielmaher01@gmail.com>
2024-03-04 22:12:13 -0500

    reverted html to original html. Refactored card functions to fit data structure of backend

--- RetireWise/frontend/src/app/cards/cards.component.ts
+++ RetireWise/frontend/src/app/cards/cards.component.ts
@@ -157,122 +157,122 @@ export class CardsComponent {
 
   /* Checks to make sure only one term and one definition are allowed to be turned over
      at the same time.*/
-  // cardTurnOverCheck(array: TermCard[] | DefinitionCard[]): boolean | undefined {
-  //   let count: number = 0;
-  //   array.forEach(function (obj) {
-  //     if (obj.isFlipped && obj.state === 'default') {
-  //       count += 1;
-  //     }
-  //   });
+  cardTurnOverCheck(array: TermCard[] | DefinitionCard[]): boolean | undefined {
+    let count: number = 0;
+    array.forEach(function (obj) {
+      if (obj.isFlipped && obj.state === 'default') {
+        count += 1;
+      }
+    });
 
-  //   if (count >= 1) {
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  // }
+    if (count >= 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-  // resetCards(termCardArray: TermCard[], definitionCardArray: DefinitionCard[]) {
-  //   let count: number = 0;
-  //   let termCardId: number = 1;
-  //   let definitionCardId: number = 0;
+  resetCards(termCardArray: TermCard[], definitionCardArray: DefinitionCard[]) {
+    let count: number = 0;
+    let termCardId: string = "d";
+    let definitionCardId: string = "s";
 
-  //   // Find all cards that are flipped in both the term array and definition array
-  //   definitionCardArray.forEach(function (definition) {
-  //     console.log(definition.definition);
-  //     if (definition.isFlipped === true && definition.state === 'default') {
-  //       definitionCardId = definition.id;
-  //       count += 1;
-  //     }
-  //   });
-  //   termCardArray.forEach(function (term) {
-  //     if (term.isFlipped === true && term.state === 'default') {
-  //       termCardId = term.id;
-  //       count += 1;
-  //     }
-  //   });
+    // Find all cards that are flipped in both the term array and definition array
+    definitionCardArray.forEach(function (definition) {
+      console.log(definition.definition);
+      if (definition.isFlipped === true && definition.state === 'default') {
+        definitionCardId = definition.id;
+        count += 1;
+      }
+    });
+    termCardArray.forEach(function (term) {
+      if (term.isFlipped === true && term.state === 'default') {
+        termCardId = term.definitionID;
+        count += 1;
+      }
+    });
 
-  //   /* if two or more cards total are flipped over and they are not a match, flip them back
-  //      over. If they are a match they will stay flipped to show the name of the term and
-  //      the definition
-  //   */
-  //   if (count >= 2) {
-  //     if (termCardId === definitionCardId) {
-  //       definitionCardArray.forEach(function (definition) {
-  //         if (definition.id === definitionCardId) {
-  //           definition.state = 'matched';
-  //         }
-  //       });
-  //       termCardArray.forEach(function (term) {
-  //         if (term.id === termCardId) {
-  //           term.state = 'matched';
-  //         }
-  //       });
-  //     } else {
-  //       definitionCardArray.forEach(function (definition) {
-  //         if (definition.state === 'default') {
-  //           definition.isFlipped = false;
-  //         }
-  //       });
-  //       termCardArray.forEach(function (term) {
-  //         if (term.state === 'default') {
-  //           term.isFlipped = false;
-  //         }
-  //       });
-  //     }
-  //   }
-  // }
+    /* if two or more cards total are flipped over and they are not a match, flip them back
+       over. If they are a match they will stay flipped to show the name of the term and
+       the definition
+    */
+    if (count >= 2) {
+      if (termCardId === definitionCardId) {
+        definitionCardArray.forEach(function (definition) {
+          if (definition.id === definitionCardId) {
+            definition.state = 'matched';
+          }
+        });
+        termCardArray.forEach(function (term) {
+          if (term.id === termCardId) {
+            term.state = 'matched';
+          }
+        });
+      } else {
+        definitionCardArray.forEach(function (definition) {
+          if (definition.state === 'default') {
+            definition.isFlipped = false;
+          }
+        });
+        termCardArray.forEach(function (term) {
+          if (term.state === 'default') {
+            term.isFlipped = false;
+          }
+        });
+      }
+    }
+  }
 
-  // // handles click event of definition cards
-  // definitionCardClicked(id: number): number | undefined {
-  //   if (this.cardTurnOverCheck(this.definitionCards) === true) {
-  //     return 0;
-  //   }
-  //   this.definitionCards.forEach(function (definition) {
-  //     if (definition.id === id) {
-  //       if (definition.isFlipped === false) {
-  //         definition.isFlipped = true;
-  //         console.log(definition.definition);
-  //       } else if (definition.state === 'matched') {
-  //         definition.isFlipped = true;
-  //       } else {
-  //         definition.isFlipped = false;
-  //       }
-  //     }
-  //   });
+  // handles click event of definition cards
+  definitionCardClicked(id: string): number | undefined {
+    if (this.cardTurnOverCheck(this.definitions) === true) {
+      return 0;
+    }
+    this.definitions.forEach(function (definition) {
+      if (definition.id === id) {
+        if (definition.isFlipped === false) {
+          definition.isFlipped = true;
+          console.log(definition.definition);
+        } else if (definition.state === 'matched') {
+          definition.isFlipped = true;
+        } else {
+          definition.isFlipped = false;
+        }
+      }
+    });
 
-  //   const myTimeout = setTimeout(
-  //     this.resetCards,
-  //     2000,
-  //     this.termCards,
-  //     this.definitionCards
-  //   );
-  //   return 0;
-  // }
+    const myTimeout = setTimeout(
+      this.resetCards,
+      2000,
+      this.terms,
+      this.definitions
+    );
+    return 0;
+  }
 
-  // // handles click event of term cards
-  // termCardClicked(id: number): number | undefined {
-  //   if (this.cardTurnOverCheck(this.termCards) === true) {
-  //     return 0;
-  //   }
-  //   this.termCards.forEach(function (term) {
-  //     if (term.id === id) {
-  //       if (term.isFlipped === false) {
-  //         term.isFlipped = true;
-  //         console.log(term.state + ' ' + term.wordName);
-  //       } else if (term.state === 'matched') {
-  //         term.isFlipped = true;
-  //       } else {
-  //         term.isFlipped = false;
-  //       }
-  //     }
-  //   });
-  //   const myTimeout = setTimeout(
-  //     this.resetCards,
-  //     2000,
-  //     this.termCards,
-  //     this.definitionCards
-  //   );
-  //   return 0;
-  // }
+  // handles click event of term cards
+  termCardClicked(id: string): number | undefined {
+    if (this.cardTurnOverCheck(this.terms) === true) {
+      return 0;
+    }
+    this.terms.forEach(function (term) {
+      if (term.id === id) {
+        if (term.isFlipped === false) {
+          term.isFlipped = true;
+          console.log(term.state + ' ' + term.wordName);
+        } else if (term.state === 'matched') {
+          term.isFlipped = true;
+        } else {
+          term.isFlipped = false;
+        }
+      }
+    });
+    const myTimeout = setTimeout(
+      this.resetCards,
+      2000,
+      this.terms,
+      this.definitions
+    );
+    return 0;
+  }
 }
