e5a446c68124d60b2fc7d8802602514a6e74ab81
Daniel Maher <danielmaher01@gmail.com>
2024-03-08 22:11:32 -0500

    created frontend page for calculator, added expense type for expense data

--- RetireWise/frontend/src/app/calculator/calculator.component.ts
+++ RetireWise/frontend/src/app/calculator/calculator.component.ts
@@ -1,12 +1,61 @@
 import { Component } from '@angular/core';
+import { Expense } from './calc_types';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-calculator',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.css'
 })
 export class CalculatorComponent {
+  expenses: Expense[] = [{
+    id: 1,
+    expense: "Kroger",
+    category: "Groceries",
+    date: "3/8/2024",
+    amount: 132.45
+
+  },
+  {
+    id: 2,
+    expense: "McDonalds",
+    category: "Dining",
+    date: "3/2/2024",
+    amount: 7.34
+  },
+  {
+    id: 1,
+    expense: "Kroger",
+    category: "Groceries",
+    date: "3/8/2024",
+    amount: 132.45
+
+  },
+  {
+    id: 2,
+    expense: "McDonalds",
+    category: "Dining",
+    date: "3/2/2024",
+    amount: 7.34
+  },
+  {
+    id: 1,
+    expense: "Kroger",
+    category: "Groceries",
+    date: "3/8/2024",
+    amount: 132.45
+
+  },
+  {
+    id: 2,
+    expense: "McDonalds",
+    category: "Dining",
+    date: "3/2/2024",
+    amount: 7.34
+  },
+  ]
 
 }
+
