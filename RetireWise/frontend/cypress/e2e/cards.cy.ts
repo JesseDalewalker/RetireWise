39e96187536a2885f4f203be6814c46e134ba40b
Dylan Tosh <dylantosh@yahoo.com>
2024-04-22 14:42:49 -0500

    Added Frontend Tests for Cards, Home, Module, and VideoAndQuizzes

--- RetireWise/frontend/cypress/e2e/cards.cy.ts
+++ RetireWise/frontend/cypress/e2e/cards.cy.ts
@@ -1,2 +1,72 @@
-describe('Matching Cards Page', () => {
+describe(`Module 1's Matching Cards Page`, () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:4200/module/1/cards');
+        cy.wait(1000);
+    });
+
+    it('Cards Page Loads', () => {
+        cy.url().should('include', 'http://localhost:4200/module/1/cards');
+    });
+
+    it('All Card Page Elements Load', () => {
+        cy.get('app-cards').should('exist');
+        cy.contains('.titles', 'Matching Card Game').should('exist');
+        cy.get('.term-container').should('exist');
+        cy.get('.definition-container').should('exist');
+        cy.get('button').contains('Done').should('exist');
+    });
+
+    it('Term Card Flips and Displays Term When Clicked', () => {
+        cy.get('.term-container').first().contains('Term');
+
+        cy.get('.term-container').first().click();
+        cy.get('.term-container').first().contains('Consumer Debt');
+    });
+
+    it('Definition Card Flips and Displays Definition When Clicked', () => {
+        cy.get('.definition-container').first().contains('Definition');
+        cy.get('.definition-container').first().click();
+        cy.get('.definition-container').first().contains('personal debts that are owed');
+    });
+
+    it('Definition and Term Cards Correctly Turn Green When Matched', () => {
+        cy.get('.term-front.term-side').first().should('have.css', 'background-color', 'rgba(0, 0, 0, 0)');
+        cy.get('.definition-front.definition-side').first().should('have.css', 'background-color', 'rgba(0, 0, 0, 0)');
+
+        cy.get('.term-container').first().click();
+        cy.get('.definition-container').first().click();
+
+        cy.get('.term-back.term-side').should('have.css', 'background-color', 'rgb(0, 128, 0)');
+        cy.get('.definition-back.definition-side').should('have.css', 'background-color', 'rgb(0, 128, 0)');
+        
+    });
+
+    it('Display Correct Message When All Cards Do Not Match', () => {
+        cy.get('button').contains('Done').click();
+
+        cy.on('window:alert', (str) => {
+            expect(str).to.equal('Please match all the cards.');
+        });
+    });
+
+    it('Display Correct Message When All Cards Match and Redirect to Module Page', () => {
+        cy.wait(1000);
+
+        cy.get('.term-container').each(($el, index) => {
+            cy.wait(1000);
+            cy.wrap($el).click();
+            cy.get('.definition-container').eq(index).click();
+            cy.wait(1000);
+        });
+
+        cy.wait(1000);
+
+        cy.get('button').contains('Done').click();
+
+        cy.on('window:alert', (str) => {
+            expect(str).to.equal('Congratulations, you matched all the cards!');
+        });
+
+        cy.url().should('include', '/module');
+    });
 })
--- RetireWise/frontend/cypress/e2e/module.cy.ts
+++ RetireWise/frontend/cypress/e2e/module.cy.ts
@@ -1,2 +1,22 @@
 describe('Module Page', () => {
+
+  it('Module Page Loads Correctly', () => {
+    cy.visit('http://localhost:4200/module')
+    cy.url().should('include', 'http://localhost:4200/module')
+  });
+
+  it('All 5 Modules Successfully Rendered as Buttons', () => {
+    cy.visit('http://localhost:4200/module')
+    cy.url().should('include', 'http://localhost:4200/module')
+  
+    cy.get('.moduleButtons').should('have.length', 5);
+  });
+
+  it(`Clicking Module 1 Button should lead to Module 1's First Video and Quiz Page`, () => {
+    cy.visit('http://localhost:4200/module')
+    cy.url().should('include', 'http://localhost:4200/module')
+
+    cy.get('.moduleButtons').first().click();
+    cy.url().should('include', 'http://localhost:4200/module/1/videoandquizzes/1');
+  });
 })
