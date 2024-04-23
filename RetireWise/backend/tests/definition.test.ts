4a5dd85b60d308c4009f00875c9ebcb25828aea3
Daniel Maher <danielmaher01@gmail.com>
2024-04-23 19:44:59 -0400

    added file to test definition endpoints

---
+++ RetireWise/backend/tests/definition.test.ts
@@ -0,0 +1,38 @@
+import * as dotenv from "dotenv";
+import request from 'supertest';
+import { Express } from 'express';
+import express from 'express';
+import { MongoClient } from 'mongodb';
+import { definitionRouter } from '../src/routes/definitions.routes';
+
+dotenv.config();
+
+const { ATLAS_URI } = process.env;
+
+if (!ATLAS_URI) {
+    console.error('The ATLAS_URI environment variable is not defined.');
+    process.exit(1);
+}
+
+let app: Express;
+let connection: MongoClient;
+
+beforeAll(async () => {
+    app = express();
+    app.use(express.json());
+    app.use('/', definitionRouter);
+    connection = await MongoClient.connect(ATLAS_URI);
+});
+
+afterAll(async () => {
+    await connection.close();
+});
+
+const agent = request.agent('http://localhost:5200');
+
+describe('GET /definitions/', () => {
+    it('Get All Definitions', async () => {
+        const response = await agent.get('/definitions');
+        expect(response.statusCode).toBe(200);
+    });
+});
